88acaf4e7912312d5484d83785c28307137c1f6e
Tahjour <tahjallman64@gmail.com>
2022-10-04 15:15:41 -0400

    Added preliminary files and page layout

---
+++ pages/events/[event-id].js
@@ -0,0 +1,6 @@
+function SpecificEventPage() {
+    return <div>
+        <h1>The Specific Event Page</h1>
+    </div>;
+}
+export default SpecificEventPage;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function StartingEventPage() {
+    return <div>
+        <h1>The Starting Event Page</h1>
+    </div>;
+}
+
+export default StartingEventPage;
