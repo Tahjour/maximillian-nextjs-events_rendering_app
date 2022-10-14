092a2ba060d20231e74b0008ce357c460746a5b9
Tahjour <tahjallman64@gmail.com>
2022-10-14 18:17:44 -0400

    added details for specific events

--- pages/events/[event-id].js
+++
@@ -1,6 +0,0 @@
-function EventDetailPage() {
-    return <div>
-        <h1>The Event Detail Page</h1>
-    </div>;
-}
-export default EventDetailPage;
---
+++ pages/events/[eventID].js
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
+import { getEventById } from "../../test-data";
+function EventDetailPage() {
+    const router = useRouter();
+    const eventId = router.query.eventID;
+    const event = getEventById(eventId);
+
+    if (!event) {
+        return <p>No event found!</p>;
+    }
+    return <Fragment>
+        <EventSummary title={event.title} />
+        <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+        <EventContent>
+            <p>{event.description}</p>
+        </EventContent>
+    </Fragment>;
+}
+export default EventDetailPage;
